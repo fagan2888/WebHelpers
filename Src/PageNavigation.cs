2d3303217b7254c93c55013d203cb3d9735b1a73
David Smith <db.ka.smith@xtra.co.nz>
2012-12-11 20:04:21 +1300

    Initial commit of helpers
    stackoverflow like paging

---
+++ Src/PageNavigation.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace dks.Web.Helpers
+{
+	public class PageNumbers
+	{
+		// todo: validate _pageLinks, should be an odd number so we can have an even number of links before and after the current page
+		private const int _pageLinks = 5;
+		private const string _prev = @"prev";
+		private const string _next = @"next";
+		private const string _dots = @"&hellip;";
+
+
+		int currentPage;
+		int pageCount;
+		string urlTemplate;
+		string cssClass;
+
+		// give [currentPage], pageCount=100, we should have links like
+		// [1] 2 3 4 5 ... 100 next
+		// prev 1 [2] 3 4 5 ... 100 next
+		// prev 1 2 [3] 4 5 ... 100 next
+		// prev 1 2 3 [4] 5 ... 100 next
+		// prev 1 ... 3 4 [5] 6 7 ... 100 next
+		// prev 1 ... 4 5 [6] 7 8 ... 100 next
+		// prev 1 ... 5 6 [7] 7 9 ... 100 next
+		// ...
+		// prev 1 ... 92 93 [94] 95 96 ... 100 next
+		// prev 1 ... 93 94 [95] 96 97 ... 100 next
+		// prev 1 ... 94 95 [96] 97 98 ... 100 next
+		// prev 1 ... 96 [97] 98 99 100 next
+		// prev 1 ... 96 97 [98] 99 100 next
+		// prev 1 ... 96 97 98 [99] 100 next
+		// prev 1 ... 96 97 98 99 [100]
+
+		public PageNumbers(int currentPage, int pageCount, string urlTemplate, string cssClass)
+		{
+			this.currentPage = currentPage;
+			this.pageCount = pageCount;
+			this.urlTemplate = urlTemplate;
+			this.cssClass = cssClass;
+		}
+
+		public override string ToString()
+		{
+
+			if (currentPage < 0) currentPage = 1;
+			if (pageCount < 0) pageCount = 0;
+
+			var pager = new PagerBuilder(urlTemplate);
+			pager.PagerClass = cssClass;
+			pager.CurrentPage = currentPage;
+
+			int start;
+			int end;
+
+			if (currentPage < _pageLinks)
+			{
+				start = 1;
+				end = _pageLinks;
+			}
+			else if (currentPage > pageCount - _pageLinks + 1)
+			{
+				start = pageCount - _pageLinks + 1;
+				end = pageCount;
+			}
+			else
+			{
+				// fixme: the +/- 2 should be calculated based on _pageLinks
+				start = currentPage - 2;
+				end = currentPage + 2;
+			}
+
+			if (currentPage > 1)
+			{
+				pager.AddPage(_prev, currentPage - 1, @"page-numbers prev");
+
+				if (start > 1)
+				{
+					pager.AddPage("1", 1, @"page-numbers");
+					if (start > 2)
+					{
+						pager.AddPage(_dots, -1, @"page-numbers dots");
+					}
+				}
+			}
+
+
+			for (var i = start; i <= end; i++)
+			{
+				if (i == currentPage)
+					pager.AddPage(i.ToString(), i, @"page-numbers current");
+				else
+					pager.AddPage(i.ToString(), i, @"page-numbers");
+			}
+
+			if (currentPage < pageCount)
+			{
+				if (end < pageCount)
+				{
+					if (end < pageCount - 1)
+					{
+						pager.AddPage(_dots, -1, @"page-numbers dots");
+					}
+					pager.AddPage(pageCount.ToString(), pageCount, @"page-numbers");
+				}
+				pager.AddPage(_next, currentPage + 1, @"page-numbers next");
+			}
+
+			return pager.ToString();
+
+		}
+
+		private class PagerBuilder
+		{
+			private class PagerLink
+			{
+				public string Title { get; set; }
+				public int PageNo { get; set; }
+				public string Class { get; set; }
+			}
+
+			private readonly string _urlTemplate;
+			private readonly List<PagerLink> _pagerLinks = new List<PagerLink>();
+
+
+			public PagerBuilder(string urlTemplate)
+			{
+				_urlTemplate = urlTemplate;
+			}
+
+			public string PagerClass { get; set; }
+
+
+
+			public void AddPage(string title, int pageNo)
+			{
+				AddPage(title, pageNo, string.Empty);
+			}
+
+			public void AddPage(string title, int pageNo, string itemClass)
+			{
+
+				var link = new PagerLink
+				{
+					PageNo = pageNo,
+					Title = title,
+					Class = itemClass
+				};
+
+				_pagerLinks.Add(link);
+			}
+
+
+
+			public override string ToString()
+			{
+				var builder = new StringBuilder();
+
+				builder.Append("<div");
+
+				if (!string.IsNullOrEmpty(PagerClass))
+				{
+					builder.Append(" class=\"");
+					builder.Append(PagerClass);
+					builder.Append("\"");
+				}
+
+				builder.Append(">");
+
+				foreach (var link in _pagerLinks)
+				{
+
+					if (link.PageNo > 0 && link.PageNo != CurrentPage)
+					{
+						builder.Append("<a href=\"");
+						builder.AppendFormat(_urlTemplate, link.PageNo);
+						builder.Append("\">");
+
+						builder.Append("<span");
+						if (!string.IsNullOrEmpty(link.Class))
+						{
+							builder.Append(" class=\"");
+							builder.Append(link.Class);
+							builder.Append("\"");
+						}
+						builder.Append(">");
+
+						builder.Append(link.Title);
+						builder.Append("</span>");
+
+						builder.Append("</a>");
+					}
+					else
+					{
+						builder.Append("<span");
+						if (!string.IsNullOrEmpty(link.Class))
+						{
+							builder.Append(" class=\"");
+							builder.Append(link.Class);
+							builder.Append("\"");
+						}
+						builder.Append(">");
+
+						builder.Append(link.Title);
+						builder.Append("</span>");
+					}
+				}
+
+				builder.Append("</div>");
+
+				return builder.ToString();
+
+			}
+
+
+			public int CurrentPage { get; set; }
+		}
+
+	}
+
+}
---
+++ Src/PageSizer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+
+namespace dks.Web.Helpers
+{
+	public class PageSizer
+	{
+		private static readonly int[] pageSizes = { 15, 30, 50 };
+
+		private string pageSizerClass;
+		private int currentPageSize;
+		private string hRef;
+
+		public PageSizer(string href, int currentPageSize, string cssClass)
+		{
+			hRef = href;
+			this.currentPageSize = currentPageSize;
+			pageSizerClass = cssClass;
+		}
+
+		public static int ValidatePageSize(int? pageSize)
+		{
+			if (pageSize.HasValue)
+			{
+				if (Array.IndexOf(pageSizes, pageSize.Value) != -1)
+				{
+					return pageSize.Value;
+				}
+			}
+
+			return DefaultPageSize;
+		}
+
+		public static int DefaultPageSize
+		{
+			get
+			{
+				return pageSizes[0];
+			}
+		}
+
+
+		public override string ToString()
+		{
+			StringBuilder builder;
+
+			builder = new StringBuilder();
+
+			//<div class="page-sizer fr">
+			//	<a class="page-numbers " title="show 15 items per page" href="&amp;pagesize=15">15</a>
+			//	<a class="page-numbers " title="show 30 items per page" href="&amp;pagesize=30">30</a>
+			//	<a class="page-numbers current" title="show 50 items per page" href="&amp;pagesize=50">50</a>
+			//	<span class="page-numbers desc">per page</span>
+			//</div>
+
+			builder.Append("<div");
+
+			if (!string.IsNullOrEmpty(pageSizerClass))
+			{
+				builder.Append(" class=\"");
+				builder.Append(pageSizerClass);
+				builder.Append("\"");
+			}
+
+			builder.Append(">");
+
+			foreach (var pageSize in pageSizes)
+			{
+				if (pageSize == currentPageSize)
+				{
+					//	<a class="page-numbers current" title="show 50 items per page" href="&amp;pagesize=50">50</a>
+					builder.AppendFormat("<span class=\"page-numbers current\" title=\"show {0} items per page\">{0}</span>", pageSize);
+				}
+				else
+				{
+					//<a class="page-numbers " title="show 15 items per page" href="&amp;pagesize=15">15</a>
+					builder.AppendFormat("<a class=\"page-numbers\" title=\"show {0} items per page\" href=\"{1}\">{0}</a>", pageSize, String.Format(hRef, pageSize));
+				}
+			}
+			builder.Append("<span class=\"page-numbers desc\">per page</span>");
+			builder.Append("</div>");
+
+			return builder.ToString();
+		}
+
+	}
+}
